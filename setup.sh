6fe8e031cf682628d73ab9c6b1502fc2b4c2acb3
Alex Graffeo-Cohen <alex.graffeocohen@shopify.com>
2022-04-18 15:03:32 -0400

    Installs rbenv and ruby via setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,19 @@
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
+# Manually install rbenv (https://github.com/rbenv/rbenv#basic-github-checkout)
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+cd ~/.rbenv && src/configure && make -C src
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(rbenv init - zsh)"' >> ~/.zshrc
+mkdir -p "$(rbenv root)"/plugins
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+source ~/.zshrc
+
+# Install Ruby into rbenv
+rbenv install 3.1.1
+rbenv global 3.1.1
+
 # Install dotfiles
 gem install homesick
 homesick clone    benjaminoakes/homesick-vi-everywhere
